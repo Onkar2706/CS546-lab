c5322f653867a79ca218519ada33b5239036d027
Onkar Mahamuni <onkar@Onkars-MacBook-Pro.local>
2024-01-26 13:28:29 -0500

    the fibonnica series updates question1

---
+++ LAB1/lab1.mjs
@@ -0,0 +1,41 @@
+export const questionOne = (index) => {
+  // Implement question 1 here
+  if (index==1)
+    return 0;
+  if (index==2)
+    return 1;
+  let index1=0
+  let index2=1
+  let result
+  while (i<index) {
+    result=index1+index2
+    index1=index2
+    index2=result
+    i=i+1
+
+    
+  }
+  return index2; //return result
+};
+
+export const questionTwo = (arr) => {
+  // Implement question 2 here
+  return; //return result
+};
+
+export const questionThree = (str) => {
+  // Implement question 3 here
+  return; //return result
+};
+
+export const questionFour = (arr) => {
+  // Implement question 4 here
+  return; //return result
+};
+
+//DO NOT FORGET TO UPDATE THE INFORMATION BELOW OR IT WILL BE -2 POINTS PER FIELD THAT IS MISSING.
+export const studentInfo = {
+  firstName: 'ONKAR',
+  lastName: 'MAHAMUNI',
+  studentId: '2022'
+};
---
+++ LAB1/lab1.test.mjs
@@ -0,0 +1,6 @@
+import * as lab1 from './lab1.mjs';
+console.log(lab1.questionOne(1))
+console.log(lab1.questionOne(2))
+
+
+//TODO: Write and call each function in lab1.js 5 times each, passing in different input
