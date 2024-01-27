dafa360e8db0509bcc6ac10739c5ff051842e0e1
Onkar <you@example.com>
2024-01-26 23:03:59 -0500

    updates test case for q1

--- LAB1/lab1.test.mjs
+++ LAB1/lab1.test.mjs
@@ -3,6 +3,7 @@ console.log(lab1.questionOne(0))
 console.log(lab1.questionOne(1))
 console.log(lab1.questionOne(2))
 console.log(lab1.questionOne(23)) 
+console.log(lab1.questionOne(100))
 
 
 //TODO: Write and call each function in lab1.js 5 times each, passing in different input
