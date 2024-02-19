9135fc2181ff5f81708f137c9f73b1910f11fb6b
Onkar <you@example.com>
2024-02-18 23:22:57 -0500

    update

--- LAB3/authors.js
+++ LAB3/authors.js
@@ -3,16 +3,17 @@
 
 //you must use axios to get the data
 import axios from "axios";
+const fetch = await axios.get('https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json')
+const authors = fetch.data
 
 export const getAuthorById = async (id) => {
     //if (id.trim === '') throw "Enter ID"
     if (id === 'string' || id.trim === '') throw "please enter valid id"
     try {
 
-        const fetch = await axios.get('https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json')
-        const authors = fetch.data
+       
 
-        let author = authors.find(function(author){
+        const author = authors.find(function(author){
             return author.id === id;
         })
 
