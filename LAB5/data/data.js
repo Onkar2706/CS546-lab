232b809def580df734e6efa1a3e9fdd5600e493b
Onkar <you@example.com>
2024-03-04 00:52:24 -0500

    lab 5 added

---
+++ LAB5/data/data.js
@@ -0,0 +1,11 @@
+/*Here, you can export the data functions
+to get the comapnies, people, getCompanyByID, getPersonById.  You will import these functions into your routing files and call the relevant function depending on the route. 
+*/
+
+const getCompanies = async () => {};
+
+const getPeople = async () => {};
+
+const getCompanyById = async (id) => {};
+
+const getPersonById = async (id) => {};
