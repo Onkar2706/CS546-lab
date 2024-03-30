b29c1a3590854e96ceb88a6e82cb9e519a1582ba
Onkar <you@example.com>
2024-03-30 18:29:25 -0400

    uopdated

--- LAB8/app.js
+++ LAB8/app.js
@@ -1,1 +1,10 @@
 //Here is where you'll set up your server as shown in lecture code
+
+// import * as  movies from "../data/movies"
+import { searchMoviesByName } from "./data/movies.js";
+try{
+    const authorData = await searchMoviesByName('Reign of Judges: Title of Liberty - Concept Short"')
+    console.log ( authorData);
+}catch(e){
+    console.log (e);
+}
--- LAB8/data/movies.js
+++ LAB8/data/movies.js
@@ -1,9 +1,35 @@
-//import axios, md5
+import axios from 'axios'
+
+const fetchMovies = await axios.get('http://www.omdbapi.com/?apikey=CS546&s={title}')
+const movies = fetchMovies.data
 
 export const searchMoviesByName = async (title) => {
   /*Function to make an axios call to search the api and return up to 20 movies matching the title param
   API endpoint: http://www.omdbapi.com/?apikey=CS546&s={title}
   */
+
+  if(title === undefined) throw "Please enter valid state"
+  if(typeof title !== 'string') throw "Please give valid string"
+  if(title.trim === '') throw "do not enter blank space"
+
+  const titles = []
+
+  for (let index = 0; index < movies.length; index++) {
+    const element4 = movies.title[index];
+    
+    const book = movies.find(function(book) {
+        return book.id === element4;
+    });
+
+    if(book) {
+        titles.push(book.title)
+    }
+
+}
+
+return titles
+
+
 };
 
 export const searchMovieById = async (id) => {
