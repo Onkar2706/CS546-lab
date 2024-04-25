0d3355ced6040c6b207b2b85f646fda1a0ac561d
ONKAR MAHAMUNI <108996739+Onkar2706@users.noreply.github.com>
2024-04-25 01:19:45 -0400

    Add files via upload
    lab10

--- LAB10/helpers.js
+++ LAB10/helpers.js
@@ -1,1 +1,377 @@
-//You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
+import bcrypt from "bcrypt";
+import { ObjectId } from "mongodb";
+
+const validate = {
+  async hashPassword(inp) {
+    const saltRounds = 10;
+    let hash = null;
+    inp = inp.trim();
+    hash = await bcrypt.hash(inp, saltRounds);
+    return hash;
+  },
+
+  checkIfProperInput: (inp) => {
+    if (!inp || inp === undefined) throw "Error: Input parameter not provided";
+  },
+
+  validatePassword: (password) => {
+    if (password.length < 8) {
+      throw new Error("Password must be at least 8 characters long");
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      throw new Error("Password must contain at least one uppercase letter");
+    }
+
+    if (!/\d/.test(password)) {
+      throw new Error("Password must contain at least one digit");
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      throw new Error("Password must contain at least one special character");
+    }
+
+    return true;
+  },
+
+  checkIfString: (inp) => {
+    validate.checkIfProperInput(inp);
+    if (typeof inp !== "string" || inp.trim().length <2 || inp.trim().length>25)
+      throw "Error: Input parameter must be non-empty string";
+  },
+  checkIfUsername: (inp) => {
+    validate.checkIfProperInput(inp);
+    if (typeof inp !== "string" || inp.trim().length <5 || inp.trim().length>=10)
+      throw "Error: Input parameter should have charcters between 5 and 10 ";
+  },
+  checkIfthemePreference: (inp) => {
+    validate.checkIfProperInput(inp);
+    if (typeof inp !== "string" || inp.trim()<=0 || inp.trim().toLowerCase() =="light" || inp.trim().toLowerCase() =="dark")
+      throw "Error: input dhould be either light or dark ";
+  },
+  checkIfRole: (inp) => {
+    validate.checkIfProperInput(inp);
+    if (typeof inp !== "string" || inp.trim()<=0 || inp.trim().toLowerCase() =="admin" || inp.trim().toLowerCase() =="user")
+      throw "Error: input dhould be either light or dark ";
+  },
+  checkIfFavoriteQuote: (inp) => {
+    validate.checkIfProperInput(inp);
+    if (typeof inp !== "string" || inp.trim().length <20 || inp.trim().length>=255)
+      throw "Error: Input parameter should have charcters between 5 and 10 ";
+  },
+  checkIfValidObjectId: (inp) => {
+    //function to check if id is a valid ObjectId
+    validate.checkIfProperInput(inp);
+    validate.checkIfString(inp);
+    inp = inp.trim();
+    if (!ObjectId.isValid(inp)) {
+      throw `Error, provided input is not a valid ObjectID`;
+    }
+  },
+  checkIfPositiveNumber: (inp) => {
+    checkIfProperInput(inp);
+    if (typeof inp !== "number" || inp === NaN)
+      throw "Error: Input parameter must be a positive number";
+    if (inp < 1) throw "Error: Price must be positive";
+    if (!Number.isInteger(inp)) {
+      let temp = inp.toString().split(".");
+      if (temp[1].length > 2) throw "Error: Only two decimal points allowed";
+    }
+  },
+
+  checkIfValidURL: (inp) => {
+    checkIfProperInput(inp);
+    inp = inp.trim();
+    let strLength = inp.length;
+    const url = new URL(inp);
+    if (strLength < 20) throw "Error: Invalid URL";
+    if (url.protocol !== "http:") throw "Error: Invalid URL";
+    if (inp.substring(strLength - 4, strLength) !== ".com")
+      throw "Error: Invalid URL";
+    if (inp.slice(11, -4).length < 5) throw "Error: Invalid URL";
+    let site = inp.slice(11, -4);
+    if (/^[a-zA-Z0-9]+([\-_\.][a-zA-Z0-9]+)*$/.test(site) === false)
+      throw "Error: Invalid URL"; // https://javascript.plainenglish.io/check-if-string-is-alphanumeric-in-javascript-e325caa3ee
+  },
+
+  checkIfValidArray: (inp) => {
+    checkIfProperInput(inp);
+    if (typeof inp !== "object" || Array.isArray(inp) !== true)
+      throw "Error: Input parameter must be an array";
+    // if (inp.length === 0) throw "Error: Empty array provided";
+    for (let string of inp) {
+      validate.checkIfString(string);
+    }
+  },
+
+  checkIfValidDate: (inp) => {
+    checkIfProperInput(inp);
+    inp = inp.trim();
+    let inpDate = new Date(inp);
+    let todayDate = new Date();
+    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (inp.length !== 10) throw "Error: Invalid date";
+    if (parseInt(inp.substring(0, 2)) < 1 || parseInt(inp.substring(0, 2)) > 12)
+      throw "Error: Invalid date"; //for checking month
+    let month = parseInt(inp.substring(0, 2));
+    if (
+      parseInt(inp.substring(3, 5)) < 1 ||
+      parseInt(inp.substring(3, 5)) > days[month - 1]
+    )
+      throw "Error: Invalid date"; //for checking day
+    if (parseInt(inp.substring(6)) > todayDate.getFullYear())
+      throw "Error: Invalid date"; //for checking year
+    if (inpDate > todayDate) throw "Error: Invalid Date"; // for checking if date in future
+  },
+
+  checkIfBoolean: (inp) => {
+    checkIfProperInput(inp);
+    if (typeof inp !== "boolean")
+      throw "Error: Input parameter must be boolean";
+  },
+
+  checkIfValidRating: (inp) => {
+    checkIfProperInput(inp);
+    if (typeof inp !== "number" || inp === NaN) throw "Error: Invalid Rating";
+    if (inp < 1 || inp > 5) throw "Error: Invalid Rating";
+    if (!Number.isInteger(inp)) {
+      let temp = inp.toString().split(".");
+      if (temp[1].length > 1)
+        throw "Error: Only one decimal points allowed for rating";
+    }
+  },
+
+  getTodayDate: () => {
+    let temp = new Date();
+    let month = temp.getMonth() + 1;
+    if (month < 10) {
+      month = "0" + month;
+    }
+    let day = temp.getDate();
+    if (day < 10) {
+      day = "0" + day;
+    }
+    let year = temp.getFullYear();
+    return `${month}/${day}/${year}`;
+  },
+
+  calculateAverageRating: (arr) => {
+    if (arr.length === 0) return 0;
+
+    let sum = null;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i]["rating"];
+    }
+    let result = sum / arr.length;
+    result = result.toFixed(1);
+    result = parseFloat(result);
+    return result;
+  },
+
+  validateState: (state) => {
+    const States = [
+      "AL",
+      "AK",
+      "AZ",
+      "AR",
+      "CA",
+      "CO",
+      "CT",
+      "DE",
+      "FL",
+      "GA",
+      "HI",
+      "ID",
+      "IL",
+      "IN",
+      "IA",
+      "KS",
+      "KY",
+      "LA",
+      "ME",
+      "MD",
+      "MA",
+      "MI",
+      "MN",
+      "MS",
+      "MO",
+      "MT",
+      "NE",
+      "NV",
+      "NH",
+      "NJ",
+      "NM",
+      "NY",
+      "NC",
+      "ND",
+      "OH",
+      "OK",
+      "OR",
+      "PA",
+      "RI",
+      "SC",
+      "SD",
+      "TN",
+      "TX",
+      "UT",
+      "VT",
+      "VA",
+      "WA",
+      "WV",
+      "WI",
+      "WY",
+    ];
+    if (state === undefined) throw "Please enter valid state";
+    if (typeof state !== "string") throw "Please give valid string";
+    if (state.length !== 2) throw "Please enter only State shortforms";
+    if (state.trim === "") throw "do not enter blank space";
+    if (!States.includes(state.toUpperCase())) throw "State is not valid";
+    return true;
+  },
+};
+
+export default validate;
+
+// // You can add and export any helper functions you want here - if you aren't using any, then you can just leave this file as is
+
+// export function validateString(str) {
+//   return typeof str === "string" && str.trim() !== "";
+// }
+
+// export function validateArray(arr) {
+//   return (
+//     Array.isArray(arr) &&
+//     arr.length > 0 &&
+//     arr.every((item) => validateString(item))
+//   );
+// }
+
+// export function validatePrice(price) {
+//   if (typeof price !== "number" || price < 1 || price === NaN) return false;
+//   if (!Number.isInteger(price)) {
+//     let parts = price.toString().split(".");
+//     if (parts[1].length > 2) return false;
+//     // return true
+//   }
+
+//   return true;
+// }
+
+// export function validateManufacturerWebsite(website) {
+//   const websiteRegex = /^http:\/\/www\.([a-zA-Z0-9]{5,})\.com$/;
+//   return (
+//     typeof website === "string" &&
+//     website.trim().startsWith("http://www.") &&
+//     website.trim().endsWith(".com") &&
+//     website.trim().length > 19 &&
+//     websiteRegex.test(website.trim())
+//   );
+// }
+
+// export function validateEmailAddress(email) {
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   return typeof email === "string" && emailRegex.test(email.trim());
+// }
+
+// export function validateDate(date) {
+//   if (!date || typeof date !== "string") {
+//     return false;
+//   }
+//   let sDate = new Date();
+//   let userDate = new Date(date);
+//   //  const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])\/\d{4}$/;
+//   //  if (!dateRegex.test(date)) return false;
+//   const [month, day, year] = date.split("/");
+//   if (userDate > sDate) return false;
+
+//   const parsedDate = new Date(`${month}/${day}/${year}`);
+//   return parsedDate && parsedDate.getMonth() == parseInt(month) - 1;
+// }
+
+// export function validateObjectId(id) {
+//   const pattern = /^[0-9a-fA-F]{24}$/;
+//   return pattern.test(id);
+// }
+
+// export function validateRating(rating) {
+//   if (rating < 1 || rating > 5) {
+//     throw new Error("Rating should be between 1 and 5");
+//   }
+//   return true;
+// }
+
+// export function validateState(state) {
+//   const States = [
+//     "AL",
+//     "AK",
+//     "AZ",
+//     "AR",
+//     "CA",
+//     "CO",
+//     "CT",
+//     "DE",
+//     "FL",
+//     "GA",
+//     "HI",
+//     "ID",
+//     "IL",
+//     "IN",
+//     "IA",
+//     "KS",
+//     "KY",
+//     "LA",
+//     "ME",
+//     "MD",
+//     "MA",
+//     "MI",
+//     "MN",
+//     "MS",
+//     "MO",
+//     "MT",
+//     "NE",
+//     "NV",
+//     "NH",
+//     "NJ",
+//     "NM",
+//     "NY",
+//     "NC",
+//     "ND",
+//     "OH",
+//     "OK",
+//     "OR",
+//     "PA",
+//     "RI",
+//     "SC",
+//     "SD",
+//     "TN",
+//     "TX",
+//     "UT",
+//     "VT",
+//     "VA",
+//     "WA",
+//     "WV",
+//     "WI",
+//     "WY",
+//   ];
+//   if (state === undefined) throw "Please enter valid state";
+//   if (typeof state !== "string") throw "Please give valid string";
+//   if (state.length !== 2) throw "Please enter only State shortforms";
+//   if (state.trim === "") throw "do not enter blank space";
+//   if (!States.includes(state.toUpperCase())) throw "State is not valid";
+//   return true
+// }
+
+// export function avgRating(rating) {
+//   if (rating.length === 0) return 0;
+//   let result = 0;
+//   for (let index = 0; index < rating.length; index++) {
+//     const element = rating[index]["rating"];
+//     result += element;
+//   }
+
+//   const average = result / rating.length;
+//   let finalAvg = average.toFixed(1);
+//   finalAvg = parseFloat(finalAvg);
+
+//   return finalAvg;
+// }
--- LAB10/routes/auth_routes.js
+++ LAB10/routes/auth_routes.js
@@ -1,36 +1,272 @@
 //import express, express router as shown in lecture code
+import express from "express";
+import validate from "../helpers.js";
+import { ObjectId } from "mongodb";
+import userMethodsDB from "../data/users.js";
+import bcrypt from "bcrypt";
+import { Router } from "express";
+import { users } from "../config/mongoCollections.js";
+import session from "express-session";
+import middleware from "../middleware.js";
 
-router.route('/').get(async (req, res) => {
+// const saltRounds = 10;
+// let hash = null;
+const router = Router();
+
+
+router.use(middleware.logRequests);
+router.use('/login', middleware.checkLoginRoute);
+router.use('/register', middleware.checkRegisterRoute);
+router.use('/user', middleware.checkUserRoute);
+router.use('/admin', middleware.checkAdminRoute);
+router.use('/logout', middleware.checkLogoutRoute);
+
+router.route("/").get(async (req, res) => {
   //code here for GET THIS ROUTE SHOULD NEVER FIRE BECAUSE OF MIDDLEWARE #1 IN SPECS.
-  return res.json({error: 'YOU SHOULD NOT BE HERE!'});
+  return res.json({ error: "YOU SHOULD NOT BE HERE!" });
+});
+
+router.route("/register").post(async (req, res) => {
+  const createUserData = req.body;
+
+  // Check if all required fields are provided
+  const requiredFields = [
+    "firstName",
+    "lastName",
+    "username",
+    "password",
+    "confirmPassword",
+    "favoriteQuote",
+    "themePreference",
+    "role",
+  ];
+  for (const field of requiredFields) {
+    if (!createUserData[field]) {
+      return res
+        .status(400)
+        .json({ Error: `Field '${field}' is missing in the request body` });
+    }
+  }
+
+  // Validate firstName
+  if (
+    createUserData.firstName.length < 2 ||
+    createUserData.firstName.length > 25
+  ) {
+    return res.status(400).json({
+      Error:
+        "Invalid firstName. It should be a valid string with a length between 2 and 25 characters",
+    });
+  }
+
+  // Validate lastName
+  if (
+    // !validate.checkIfString(createUserData.lastName)
+    createUserData.lastName.length < 2 ||
+    createUserData.lastName.length > 25
+  ) {
+    return res.status(400).json({
+      Error:
+        "Invalid lastName. It should be a valid string with a length between 2 and 25 characters",
+    });
+  }
+
+  // Validate username
+  if (
+    // !validate.checkIfString(createUserData.username) ||
+    createUserData.username.length < 5 ||
+    createUserData.username.length > 10
+  ) {
+    return res.status(400).json({
+      Error:
+        "Invalid username. It should be a valid string with a length between 5 and 10 characters",
+    });
+  }
+
+  // // Validate password
+  if (!validate.validatePassword(createUserData.password)) {
+    return res.status(400).json({
+      Error:
+        "Invalid password. It should be a valid string with at least one uppercase character, one number, and one special character, and a minimum length of 8 characters",
+    });
+  }
+
+  // // Validate confirmPassword
+  if (createUserData.password !== createUserData.confirmPassword) {
+    return res.status(400).json({ Error: "Passwords do not match" });
+  }
+
+  // // Validate favoriteQuote
+  if (
+    // !validate.checkIfString(createUserData.favoriteQuote) ||
+    createUserData.favoriteQuote.length < 20 ||
+    createUserData.favoriteQuote.length > 255
+  ) {
+    return res.status(400).json({
+      Error:
+        "Invalid favoriteQuote. It should be a valid string with a length between 20 and 255 characters",
+    });
+  }
+
+  // // Validate themePreference
+  if (
+    createUserData.themePreference !== "light" &&
+    createUserData.themePreference !== "dark"
+  ) {
+    return res.status(400).json({
+      Error: "Invalid themePreference. It should be either 'light' or 'dark'",
+    });
+  }
+
+  // // // Validate role
+  if (createUserData.role !== "admin" && createUserData.role !== "user") {
+    return res
+      .status(400)
+      .json({ Error: "Invalid role. It should be either 'admin' or 'user'" });
+  }
+
+  try {
+    // Call registerUser DB function
+    const newUser = await userMethodsDB.registerUser(
+      createUserData.firstName,
+      createUserData.lastName,
+      createUserData.username,
+      createUserData.password,
+      createUserData.favoriteQuote,
+      createUserData.themePreference,
+      createUserData.role
+    );
+
+    if (newUser.signupCompleted) {
+      return res.redirect("/login"); // Redirect user to login page
+    } else {
+      return res.status(500).json({ Error: "Internal Server Error" });
+    }
+  } catch (error) {
+    return res.status(500).json({ Error: "Internal Server Error" });
+  }
+});
+router.route("/register").get(async (req, res) => {
+  res.render("register", { title: "Sign Up" });
 });
 
 router
-  .route('/register')
+  .route("/login")
   .get(async (req, res) => {
     //code here for GET
+    res.render("login", { title: "Login" });
   })
   .post(async (req, res) => {
     //code here for POST
+    const { username, password } = req.body;
+
+    // Check if username and password are supplied
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ Error: "Username and password are required" });
+    }
+
+    // Validate username
+    const usernameRegex = /^[a-zA-Z\s]*$/; // Only letters and spaces
+    if (
+      !usernameRegex.test(username) ||
+      username.length < 5 ||
+      username.length > 10
+    ) {
+      return res.status(400).json({ Error: "Invalid username format" });
+    }
+
+    // Validate password
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // At least one uppercase, one lowercase, one digit, one special character, minimum length 8
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({ Error: "Invalid password format" });
+    }
+
+    // console.log(req.session);
+
+    try {
+      // Call loginUser DB function
+      const users = await userMethodsDB.loginUser(username, password);
+
+      // Check if user is found and passwords match
+      if (users) {
+        // Store user information in session
+
+        req.session.user = {
+          firstName: users.firstName,
+          lastName: users.lastName,
+          username: users.username,
+          favoriteQuote: users.favoriteQuote,
+          themePreference: users.themePreference,
+          role: users.role,
+        };
+
+        // console.log(req.session.user);
+      } else {
+        return res.status(400).json({ Error: "Invalid username or password" });
+      }
+      if (users.role == "admin") {
+        return res.redirect("/admin");
+      } else {
+        return res.redirect("/user");
+      }
+    } catch (error) {
+      // console.error(error);
+      return res.status(500).json({ Error: "Internal Server Error" });
+    }
   });
 
-router
-  .route('/login')
-  .get(async (req, res) => {
-    //code here for GET
-  })
-  .post(async (req, res) => {
-    //code here for POST
+router.route("/user").get(async (req, res) => {
+  //code here for GET
+  const user = req.session.user;
+
+  const currentTime = new Date().toLocaleTimeString();
+
+  return res.render("user", {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    currentTime: currentTime,
+    role: user.role,
+    favoriteQuote: user.favoriteQuote,
+    themePreference: user.themePreference
   });
-
-router.route('/user').get(async (req, res) => {
-  //code here for GET
 });
 
-router.route('/admin').get(async (req, res) => {
+router.route("/admin").get(async (req, res) => {
   //code here for GET
+
+  if (req.session.user && req.session.user.role === "admin") {
+    const user = req.session.user;
+    const currentTime = new Date().toLocaleString(); // Get current time
+
+    // Render the admin view with user information
+    res.render("admin", {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      currentTime: currentTime,
+      favoriteQuote: user.favoriteQuote,
+      role: user.role,
+      themePreference: user.themePreference
+    });
+  } else {
+    // User is not authenticated or is not an admin, send forbidden status
+    res.status(403).send("Access forbidden");
+  }
 });
 
-router.route('/logout').get(async (req, res) => {
+router.route("/logout").get(async (req, res) => {
   //code here for GET
+  req.session.destroy(err => {
+    if (err) {
+      // console.error('Error destroying session:', err);
+      res.status(500).send('Error logging out');
+    } else {
+      // Inform the user that they have been logged out
+      res.send('You have been logged out. <a href="/login">Click here to login</a>');
+    }
+  });
 });
+
+export default router;
