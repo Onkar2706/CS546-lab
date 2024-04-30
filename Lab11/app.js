4c0c9471ea808df7fb39412eb3ab8cda4f9a6162
Onkar <you@example.com>
2024-04-30 17:46:46 -0400

    lab11 done

--- Lab11/app.js
+++ Lab11/app.js
@@ -1,3 +1,19 @@
 // Code server here
 // Your server this week should not do any of the processing or calculations
 // Your server only exists to allow someone to get to the HTML Page and download the associated assets to run the application
+import express from 'express';
+const staticdir = express.static('public')
+const app = express();
+import configRoutesFunction from './routes/index.js';
+
+
+app.use('/public',staticdir)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+configRoutesFunction(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
--- Lab11/routes/index.js
+++ Lab11/routes/index.js
@@ -1,1 +1,14 @@
 //Here you will require route files and export the constructor method as shown in lecture code and worked in previous labs.
+
+import routesApi from './routesApi.js';//
+const constructorMethod = (app) => {
+    app.use('/',routesApi)
+    app.use('*',(req,res)=>{
+      res.redirect('/')
+    })
+  };
+  
+  
+  
+  
+export default constructorMethod;
